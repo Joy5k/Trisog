047b9ef135b4d9a1a72959c717c8380e2a014eb4
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2023-11-16 04:21:16 +0600

    added the trip plan

--- src/Comopnents/TripPlan.jsx
+++ src/Comopnents/TripPlan.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import banner from '../assets/Home-banner.png'
+import { GiCheckMark } from "react-icons/gi";
+
 const TripPlan = () => {
     return (
         <div className='mt-36'>
@@ -15,7 +17,16 @@ const TripPlan = () => {
       <h1 className="text-5xl font-bold text-black">With Advancers</h1> <br />
                         <p className='text-black '>Find awesome hotel,tour,car and Activities <br />
      consectetur adipisicing elit. Autem fugit temporibus vero beatae officiis magni nam dolore amet veritatis eius. Quibusdam accusamus eos ipsa, quas cupiditate a placeat sequi asperiores.                    </p>
-    </div>
+   
+                        <div className='grid grid-cols-2 my-5'>
+  <p className='flex align-middle font-bold text-[#353F5D]'> <span><GiCheckMark className='mt-1 mr-1' /></span> <span>Travel Plan</span></p>
+  <p className='text-[#353F5D] flex align-middle font-bold'> <span><GiCheckMark className='mt-1 mr-1' /></span> <span>Cheap Rates</span></p>
+  <p className='text-[#353F5D] flex align-middle font-bold'> <span><GiCheckMark className='mt-1 mr-1' /></span> <span>Hand-Pick Tour</span></p>
+  <p className='text-[#353F5D] flex align-middle font-bold'> <span><GiCheckMark className='mt-1 mr-1' /></span> <span>Private Guide</span></p>
+                  
+                        </div>
+                        <button className='p-6 py-3 mt-2 rounded-lg border border-black'>Contact Us</button>
+                    </div>
     
   </div>
             </div>
