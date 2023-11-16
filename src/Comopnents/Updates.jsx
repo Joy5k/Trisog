e2767db827267b0a01c4227c34a73c907c082ff0
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2023-11-16 13:53:52 +0600

    complete the home page

--- src/Comopnents/Updates.jsx
+++ src/Comopnents/Updates.jsx
@@ -3,6 +3,11 @@ import img1 from "../assets/travel1.png"
 import img2 from "../assets/travel2.png"
 import img3 from "../assets/travel3.png"
 import img4 from "../assets/travel4.png"
+import advencer from "../assets/advencer1.png"
+import advencer2 from "../assets/advencer2.png"
+import advencer3 from "../assets/advencer3.png"
+import advencer4 from "../assets/advencer4.png"
+import advencer5 from "../assets/advencer5.png"
 const Updates = () => {
     return (
         <div className='my-10 w-[1240px] mx-auto'>
@@ -44,7 +49,16 @@ const Updates = () => {
   </div>
             </div>
         </div>
-        
+            <hr />
+
+    {/* Adding 4 extra Image  */}
+            <div className='grid sm:grid-cols-3 lg:grid-cols-5 px-10 mx-auto mt-10'>
+        <img src={advencer} alt="" />
+        <img src={advencer2} alt="" />
+        <img src={advencer3} alt="" />
+        <img src={advencer4} alt="" />
+        <img src={advencer5} alt="" />
+            </div>
         </div>
     );
 };
